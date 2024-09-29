83d8010374f366b1ebb4658cbd34ad03eaea3233
Jake <jakebailes76@yahoo.com>
2024-09-29 03:35:48 -0400

    cross region

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors())
 
+const v5region = 'americas'
+const v4region = 'na1'
+
 const apiKey = process.env.API_KEY // Use environment variable
 
 app.get('/api/account/:gameName/:tagLine', async (req, res) => {
@@ -15,7 +18,7 @@ app.get('/api/account/:gameName/:tagLine', async (req, res) => {
 
   try {
     const response = await fetch(
-      `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${apiKey}`
+      `https://${v5region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${apiKey}`
     )
     if (response.ok) {
       const data = await response.json()
@@ -33,7 +36,7 @@ app.get('/api/matches/:puuid', async (req, res) => {
   const { puuid } = req.params
   try {
     const response = await fetch(
-      `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=10&api_key=${apiKey}`
+      `https://${v5region}.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=10&api_key=${apiKey}`
     )
     if (response.ok) {
       const data = await response.json()
@@ -50,7 +53,7 @@ app.get('/api/poop/id/:puuid', async (req, res) => {
   const { puuid } = req.params
   try {
     const response = await fetch(
-      `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${apiKey}`
+      `https://${v4region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${apiKey}`
     )
     if (response.ok) {
       const data = await response.json()
@@ -68,7 +71,7 @@ app.get('/api/match/:matchId', async (req, res) => {
   const { matchId } = req.params
   try {
     const response = await fetch(
-      `https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${apiKey}
+      `https://${v5region}.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${apiKey}
       `
     )
     if (response.ok) {
@@ -87,7 +90,7 @@ app.get('/api/league/:id', async (req, res) => {
   const { id } = req.params
   try {
     const response = await fetch(
-      `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${apiKey}
+      `https://${v4region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${apiKey}
 
       `
     )
